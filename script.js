005ec1ba346cfaf9a127002e962681e8cf824333
David Alexii <davidalexii2010@gmail.com>
2026-01-03 00:53:32 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const button1 = document.getElementById("smth");
+const button2 = document.getElementById("smth2");
+const counter = document.getElementById("counter-display");
+let cookieNumber = 0;
+
+button1.addEventListener("click", increaseCounter);
+button2.addEventListener("click", decreaseCounter);
+
+function increaseCounter() {
+  button1.style.padding = "1rem 2rem";
+  button2.style.padding = "10px";
+  cookieNumber++;
+  counter.innerHTML = cookieNumber;
+}
+
+function decreaseCounter() {
+  button2.style.padding = "1rem 2rem";
+  button1.style.padding = "10px";
+  if (cookieNumber > 0) {
+    cookieNumber--;
+  }
+  counter.innerHTML = cookieNumber;
+  if (cookieNumber == 0) {
+    counter.innerText = "No more cookies :(";
+  }
+}
